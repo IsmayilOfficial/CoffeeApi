b319766f5f4ea851d2b08045a64c1352c381eac3
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-02-10 23:51:38 +0400

    first commit

---
+++ coffeeManiac/src/test/java/com/example/coffeeManiac/CoffeeManiacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeManiac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeManiacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
